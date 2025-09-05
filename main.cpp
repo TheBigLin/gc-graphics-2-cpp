74ed32fb91af08c61a5eb2755bb805acff4aeafd
Hayden Linford <200601814@student.georgianc.on.ca>
2025-09-05 09:15:57 -0400

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <Windows.h>
+
+
+int main()
+{
+	int x = 1;
+	int y = 2;
+	int z = 3;
+
+	// std::cout << x << std::endl;
+	// std::cout << y << std::endl;
+	// std::cout << z << std::endl;
+	while (true)
+	{
+		SetCursorPos(0, 0);
+	}
+	return 69420;
+}
