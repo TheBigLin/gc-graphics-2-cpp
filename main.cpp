d797bce99910c9103e78bc561c81675a8ea7f2d7
Hayden Linford <200601814@student.georgianc.on.ca>
2025-09-12 08:35:43 -0400

    Week2 part 1

--- main.cpp
+++ main.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
-#include <Windows.h>
-
 
 int main()
 {
-	int x = 1;
-	int y = 2;
-	int z = 3;
+	char text[5][10];
 
-	// std::cout << x << std::endl;
-	// std::cout << y << std::endl;
-	// std::cout << z << std::endl;
-	//while (true)
-	//{
-	//	SetCursorPos(0, 0);
-	//}
-	return 69420;
+	for (int y = 0; y < 5; y++)
+	{
+		for (int x = 0; x < 10; x++)
+		{
+			text[y][x] = '*';
+			std::cout << text[y][x];
+		}
+		std::cout << std::endl;
+	}
+
+	return 0;
 }
