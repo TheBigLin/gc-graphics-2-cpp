aab965756f5a3901db7cbda125a3d60fe996a7b2
TheBigLin <Hayden22@live.ca>
2025-09-19 08:59:28 -0400

    part of week 3 lesson (left for Gen ED)

--- main.cpp
+++ main.cpp
@@ -1,61 +1,25 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
-// 1, create a person object that stores a name and age.
-
-// 2, Create an array of people. Ensure each person has a name and age.
-
-// 3, Loop through people. Log the name and age of each person.
-
-
-struct Person
-{
-	int age;
-	std::string name;
-
-};
+void Example1();
 
 int main()
 {
-	
-	const int peopleCount = 3;
-	Person people[peopleCount];
-	people[0].name = "connor";
-	people[0].age = 27;
-	people[1].name = "Hayden";
-	people[1].age = 19;
-	people[2].name = "BigLin";
-	people[2].age = 999;
+	std::vector<int> numbers;
+	numbers.resize(10);
 
-
-	for (int i = 0; i < peopleCount; i++)
-	{
-		std::string name = people[i].name;
-		int age = people[i].age;
-		std::cout << "Name:" << people[i].name << " age:" << age << std::endl;
-	}
-
-	
-
-
-	//std::array<Person, 3> persons;
-	
-	//Person person;
-	//person.name = "Connor";
-	//person.age = 27;
-	//person.name = "Hayden";
-	//person.age = 19;
-	//person.name = "Lebron";
-	//person.age = 40;
-	//person.name = "BigLin";
-	//person.age = 999;
-	//person.name = "Person5";
-	//person.age = 21;
-	//const int count = 5;
-
-	//Person 
-
-	// make an array of people instead of numbers
-	// int numbers [count];
 	return 0;
+}
+
+
+void Example1()
+{
+	std::vector<int> numbers;
+	numbers.push_back(1);
+	numbers.push_back(2);
+	numbers.push_back(3);
+	//numbers.pop_back();
+	//numbers.pop_back();
+	//numbers.pop_back();
 }
